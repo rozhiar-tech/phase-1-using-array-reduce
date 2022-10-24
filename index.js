a22f35e56b7f3dd2e7e4f690c302889e24aa219a
rozhiar <rozyar.313175000@uhd.edu.iq>
2022-10-24 13:00:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+// function totalBatteries(arr) {
+  const totalBatteries = batteryBatches.reduce((previous, currentValue, index) => {
+    return previous + currentValue
+
+  })
+  
+//   return  totalBatteries
+// }
+
+// console.log(totalBatteries(batteryBatches));
+console.log(totalBatteries);
